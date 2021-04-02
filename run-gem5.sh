66021ac64816ea25ab570259803b24e753eca0e6
Eric Wu <tywu13@bwrcr740-8.eecs.berkeley.edu>
2021-04-02 13:59:40 -0700

    add support for arm

--- run-gem5.sh
+++ run-gem5.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-TARGET_RUN="/tools/B/tywu13/gem5/build/X86/gem5.opt"
+TARGET_RUN="/tools/B/tywu13/gem5/build/ARM/gem5.opt"
 GEM5_CONFIG="/tools/B/tywu13/gem5/configs/example/se.py"
 GEM5_OPTIONS="--l1d_size=64kB --l1i_size=16kB --caches"
 
-INPUT_TYPE=ref # THIS MUST BE ON LINE 4 for an external sed command to work!
+INPUT_TYPE=test # THIS MUST BE ON LINE 4 for an external sed command to work!
                 # this allows us to externally set the INPUT_TYPE this script will execute
+LABEL=arm # given the cfg file's label
+if [[ $LABEL == "arm" ]]; then
+   label_suffix="64"
+else
+   label_suffix="m64"
+fi
 
 suite_type=intspeed
 if [[ $suite_type == *"speed"* ]]; then
@@ -21,8 +27,8 @@ else
 fi
 
 if [[ $suite_type == "intspeed" ]]; then
-    BENCHMARKS=(600.perlbench 602.gcc)
-    # 605.mcf 620.omnetpp 623.xalancbmk 625.x264 631.deepsjeng 641.leela 648.exchange2 657.xz)
+    #BENCHMARKS=(600.perlbench 602.gcc)
+    BENCHMARKS=(605.mcf 620.omnetpp 623.xalancbmk 625.x264 631.deepsjeng 641.leela 648.exchange2 657.xz)
 elif [[ $suite_type == "intrate" ]]; then
     BENCHMARKS=()
 else
@@ -36,13 +42,15 @@ overlay_dir=$PWD/build/overlay
 
 for b in ${BENCHMARKS[@]}; do
    # specify the directory that gem5 will dump the stats
-   rm -rf ${base_dir}/output/${b}
-   mkdir -p ${base_dir}/output/${b}
-   echo " -== Dumping ${b} stats in ${base_dir}/output/${b}  ==-"
+   OUT_DIR=${base_dir}/output/${b}/${LABEL}/${INPUT_TYPE}
+   mkdir -p ${OUT_DIR}
+   echo " -== Dumping ${b} stats in ${OUT_DIR} ==-"
+
     
    # go to the directory where the exectuabl&inputs reside
-   echo "cd ${overlay_dir}/$suite_type/${b}${suffix}/${INPUT_TYPE}"
-   cd ${overlay_dir}/$suite_type/${b}${suffix}/${INPUT_TYE}
+   BIN_DIR=${overlay_dir}/$suite_type/${b}${suffix}/${LABEL}/${INPUT_TYPE}
+   echo "cd ${BIN_DIR}"
+   cd ${BIN_DIR}
    SHORT_EXE=${b##*.}$suffix # cut off the numbers ###.short_exe
    if [ $b == "602.gcc" ]; then 
       SHORT_EXE=sgcc #WTF SPEC???
@@ -59,7 +67,7 @@ for b in ${BENCHMARKS[@]}; do
    input=${input[@]::${#input[@]}-4}
    # echo $input
    if [[ ${input:0:1} != '#' ]]; then # allow us to comment out lines in the cmd files
-       cmd="${TARGET_RUN} --outdir=${base_dir}/output/${b} $GEM5_CONFIG -c ${SHORT_EXE}_base.mytest-m64 --options=\"${input}\" $GEM5_OPTIONS"
+       cmd="${TARGET_RUN} --outdir=${OUT_DIR} $GEM5_CONFIG -c ${SHORT_EXE}_base.${LABEL}-${label_suffix} --options=\"${input}\" $GEM5_OPTIONS"
        echo "workload=[${cmd}]"
        eval ${cmd}
        # ((count++)) 
